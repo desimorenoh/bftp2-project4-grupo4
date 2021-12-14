fee952ff8b958bb4c57e0dd74ebf252138bb6f53
AnaCasasC <cervelloana@gmail.com>
2021-12-14 10:34:39 +0100

    first commit

---
+++ src/main/java/org/factoriaf5/bftp2project4grupo4/Bftp2Project4Grupo4Application.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.bftp2project4grupo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bftp2Project4Grupo4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bftp2Project4Grupo4Application.class, args);
+    }
+
+}
---
+++ src/main/java/org/factoriaf5/bftp2project4grupo4/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package org.factoriaf5.bftp2project4grupo4.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+    public class HomeController {
+        @GetMapping("/")
+        public String home() {
+            return "home";
+        }
+    }
+
