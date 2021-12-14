14a6fad7753a78970aa15f5fa6c04e46ed1eac27
AnaCasasC <cervelloana@gmail.com>
2021-12-14 14:58:28 +0100

    creando juegos controller

---
+++ src/main/java/org/factoriaf5/bftp2project4grupo4/controllers/JuegoController.java
@@ -0,0 +1,4 @@
+package org.factoriaf5.bftp2project4grupo4.controllers;
+
+public class JuegoController {
+}
---
+++ src/main/java/org/factoriaf5/bftp2project4grupo4/repositories/Juego.java
@@ -0,0 +1,4 @@
+package org.factoriaf5.bftp2project4grupo4.repositories;
+
+public class Juego {
+}
